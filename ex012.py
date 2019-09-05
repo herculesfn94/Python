7a06715dc66096b93f1d43c11d9c70c67830225c
Hercules Franco <52856686+herculesfn94@users.noreply.github.com>
2019-09-05 13:51:19 -0300

    Rename ex012.py to ex012.py - Alterando preço de produtos

--- ex012.py
+++
@@ -1,3 +0,0 @@
-preco = float(input('Qual o preco do produto? R$ '))
-novo = preco - (preco*5 / 100)
-print('O pruduto que custava R${:.2f}, com 5% de desconto vai custar R${:.2f}'.format(preco, novo))
